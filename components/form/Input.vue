8dd40ab3e6e64b1a58c4c746b42d9241c582a93d
= <bjoern.pfoster@gmail.com>
2019-08-19 21:58:07 +0200

    Adjusted input component

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="lg:flex lg:items-center my-6">
-    <div class="lg:w-1/3">
-      <label class="block text-gray-500 lg:text-right mb-1 lg:mb-0 pr-4"
-             :for="id">
-        {{ label }}
-      </label>
-    </div>
+  <div>
+    <div class="lg:flex lg:items-center"
+         :class="{'my-6': errors.length === 0, 'mt-6': errors.length !== 0}">
+      <div class="lg:w-1/3">
+        <label class="block text-gray-500 lg:text-right mb-1 lg:mb-0 pr-4"
+               :for="id">
+          {{ label }}
+        </label>
+      </div>
 
-    <div class="lg:w-2/3">
-      <input :id="id"
-             :value="value"
-             class="input"
-             :type="type"
-             @input="onInput" />
+      <div class="lg:w-2/3">
+        <input :id="id"
+               ref="input"
+               v-model="inputValue"
+               :class="['input', classes]"
+               :placeholder="placeholder"
+               :type="type"
+               @input="onInput"
+               @blur="onBlur" />
+      </div>
+    </div>
+    <div
+      v-for="(error, i) in errors"
+      :key="i"
+      class="mb-6 flex">
+      <div class="hidden lg:block lg:w-1/3">
+        &nbsp;
+      </div>
+      <div class="w-full lg:w-2/3 text-center error">
+        {{ error }}
+      </div>
     </div>
   </div>
 </template>
@@ -20,19 +37,17 @@
 <script>
 export default {
   name: 'Input',
+  model: {
+    event: 'modified',
+  },
   props: {
-    type: {
-      default: 'text',
-      type: String,
-      require: false,
-    },
-    value: {
-      default: '',
+    id: {
+      default: 'input',
       type: String,
       required: true,
     },
-    id: {
-      default: 'input',
+    value: {
+      default: '',
       type: String,
       required: true,
     },
@@ -41,18 +56,52 @@ export default {
       type: String,
       required: true,
     },
+    type: {
+      default: 'text',
+      type: String,
+      require: false,
+    },
+    validate: {
+      default: () => [],
+      type: Function,
+      required: false,
+    },
+    placeholder: {
+      default: '',
+      type: String,
+      required: false,
+    },
+  },
+  data: function () {
+    return {
+      touched: false,
+      classes: '',
+      errors: [],
+      inputValue: this.value,
+    };
   },
   methods: {
+    onBlur() {
+      this.touched = true;
+      this.emit();
+    },
     onInput(event) {
-      this.$emit('input', event.target.value);
-    }
-    ,
-  }
-  ,
-}
-;
+      event.preventDefault();
+      this.$emit('modified', this.inputValue);
+
+      if (!this.touched) {
+        this.classes = '';
+        return;
+      }
+      this.emit();
+    },
+    emit() {
+      const result = new Promise(resolve => this.$emit('validate', resolve));
+      result.then((errors) => {
+        this.errors = errors;
+        this.classes = this.errors.length === 0 ? 'ok' : 'error';
+      });
+    },
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
