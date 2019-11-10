35bb28fe702108014ddb974dd9c0c179f4870579
= <bjoern.pfoster@gmail.com>
2019-11-10 22:59:30 +0100

    Added file input

---
+++ components/form/FileInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="file-input"
+       @click="select()">
+    <div :class="css">
+      <span v-if="value">Selected File: {{ value.name }}</span>
+      <span v-else>Select File</span>
+    </div>
+    <input ref="file"
+           type="file"
+           class="hidden"
+           @change="handleFileChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileInput',
+  props: {
+    value: File,
+    css: {
+      type: String,
+      default: 'button',
+    },
+  },
+  methods: {
+    select() {
+      this.$refs.file.click();
+    },
+    handleFileChange(e) {
+      this.$emit('input', e.target.files[0]);
+    },
+  },
+};
+</script>
