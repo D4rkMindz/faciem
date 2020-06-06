757e47ef00c6d6ed02cbcb0e054bbdd84c52cdf5
= <bjoern.pfoster@gmail.com>
2020-06-06 18:29:41 +0200

    Added footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    <navbar />
-    <div class="mt-8">
+  <div class="flex flex-col min-h-screen">
+    <navbar class="flex-grow-0" />
+    <div class="mt-8 flex-grow">
       <nuxt />
     </div>
+    <footer class="text-white mt-5 flex-grow-0">
+      <div class="bg-purple-700 w-full">
+        <div class="container mx-auto px-2 md:px-0 pt-5 pb-3 flex flex-col md:flex-row">
+          <div class="w-1 md:w-1/3 lg:w-1/6 flex flex-col">
+            <h3 class="font-bold">
+              {{ $t('FOOTER.first.title') }}
+            </h3>
+            <nuxt-link :to="localeRoute('/imprint')"
+                       class="mt-1">
+              {{ $t('FOOTER.first.imprint') }}
+            </nuxt-link>
+            <nuxt-link :to="localeRoute('/contact')"
+                       class="mt-1">
+              {{ $t('FOOTER.first.contact') }}
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="bg-purple-900 w-full">
+        <div class="container mx-auto bg-purple-900 py-2">
+          © 2019 - {{ currentYear }} venovum
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 <script>
+import moment from 'moment';
 import Navbar from '~/components/layout/Navbar';
 
 export default {
   components: { Navbar },
+  computed: {
+    currentYear() {
+      return moment().year().toString();
+    },
+  },
 };
 </script>
 
---
+++ pages/contact/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="font-bold text-2xl">
+      {{ $t('CONTACT.title') }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage',
+};
+</script>
